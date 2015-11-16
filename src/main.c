b2d0e59a28d69b7af8683037b70b66821188087c
majorabora <jacoblockhart@gmail.com>
2015-11-16 23:33:11 +0000

    filled out ticks in extops instructon table

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include "../include/cartridge.h"
 #include "../include/timer.h"
 #include "../include/bitUtils.h"
+#include "../include/extops.h"
 
 #include "../include/interrupt.h"
 
@@ -18,8 +19,10 @@ int main(void)
 	gameboy = createGameboy();
 
 	loadGame(gameboy, "../games/tetris.gb");
-
-	startEmulationLoop(gameboy);
+	for (int i = 0; i < 0x100; i++){
+		executeExtendedOpcode(gameboy, i);
+	}
+	//startEmulationLoop(gameboy);
 
 	return 0;
 
